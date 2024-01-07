67c78c92d8c800bf9bb92f3f1e842434d6a216c4
qlaueen <qlaueen@gmail.com>
2024-01-06 19:17:58 -0500

    first commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require("@tailwindcss/forms"),
+  ],
+}
+
